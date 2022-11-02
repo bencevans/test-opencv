bbd59fce2eca3e643ebd4ed84900223fb3dd9c20
Ben Evans <ben@bluechimp.io>
2022-11-02 18:36:48 +0000

    lib search

--- build.rs
+++ build.rs
@@ -3,20 +3,23 @@ use std::env::var;
 fn main() {
     let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
 
-    println!("cargo:rustc-link-search={}/opencv/build/3rdparty/lib", manifest_dir);
-    println!("cargo:rustc-link-lib=ittnotify");
-    println!("cargo:rustc-link-lib=libjpeg-turbo");
-    println!("cargo:rustc-link-lib=libpng");
-    println!("cargo:rustc-link-lib=libprotobuf");
-    println!("cargo:rustc-link-lib=zlib");
+    let lib_dir = format!("{}/opencv/build/3rdparty/lib", manifest_dir);
 
-    if cfg!(target_arch = "aarch64") {
-        println!("cargo:rustc-link-lib=tegra_hal");
+    println!("cargo:rustc-link-search={}", lib_dir);
+
+    for lib_file in std::fs::read_dir(lib_dir).unwrap() {
+        let entry = lib_file.unwrap();
+        let lib_path = entry.path();
+        let lib = lib_path.file_stem().unwrap().to_str().unwrap();
+
+        println!(
+            "cargo:rustc-link-lib={}",
+            lib.strip_prefix("lib").unwrap().replace(".a", "")
+        );
     }
 
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=framework=OpenCL");
         println!("cargo:rustc-link-lib=framework=Accelerate");
     }
-
 }
