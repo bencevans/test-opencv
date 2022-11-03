7b03476ed05a6995948ba8267c7ce3878b36ba7d
Ben Evans <ben@bluechimp.io>
2022-11-03 00:51:01 +0000

    vcpkg

--- build.rs
+++ build.rs
@@ -1,23 +1,4 @@
-use std::{env::var, path::PathBuf};
-
 fn main() {
-    let manifest_dir = var("CARGO_MANIFEST_DIR").unwrap();
-
-    let lib_dir = PathBuf::from(manifest_dir).join(PathBuf::from("opencv\\build\\3rdparty\\lib\\Debug"));
-
-    println!("cargo:rustc-link-search={}", lib_dir.to_str().unwrap());
-
-    for lib_file in std::fs::read_dir(lib_dir).unwrap() {
-        let entry = lib_file.unwrap();
-        let lib_path = entry.path();
-        let lib = lib_path.file_stem().unwrap().to_str().unwrap();
-
-        println!(
-            "cargo:rustc-link-lib={}",
-            lib.strip_prefix("lib").unwrap()
-        );
-    }
-
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=framework=OpenCL");
         println!("cargo:rustc-link-lib=framework=Accelerate");
